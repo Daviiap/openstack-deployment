95575371e01c30a56cec887b37ebf541b0f2226d
Fernando Marques <fernando.silva@ccc.ufcg.edu.br>
2021-09-27 12:25:31 -0300

    add função deploy

--- src/main.py
+++ src/main.py
@@ -50,10 +50,7 @@ def program(flavorController, imageController, securityGroupController, networkC
         elif option == '3':
             status = delete_instance(serverController=serverController)
         elif option == '4':
-            print('connecting ssh...')
-            # ssh = paramiko.SSHClient()
-            # ssh ubuntu@10.11.19.72 -i ../VPN/cloudKeys/cloud.key
-            # ssh.connect('10.11.19.72', username='<username>', password='<password>', key_filename='../VPN/cloudKeys/cloud.key')
+            system('bash ./deploy/config.sh')
 
         if instance_infos != None:
             system('clear')
@@ -88,7 +85,7 @@ def main():
     errorController = ErrorController(openstack_conn)
 
     system('clear')
-    #print('Lista de servers já existentes na cloud \'' + cloud + '\':')
+    print('Lista de servers já existentes na cloud \'' + cloud + '\':')
     print_table(serverController.list_servers())
     input('Pressione Enter para continuar...')
 
